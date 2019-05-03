521c5eba200bb51e4805a93af7e261f37d9a21db
Cara Ponzini <ponzini@gmail.com>
2019-05-03 09:00:42 -0700

    prints stuff

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const Koa = require('koa');
-const app = new Koa();
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
 const port = process.env.PORT || 7890;
+const path = require('path');
+const render = require('koa-ejs'); //template engine
 
-app.use(async ctx => ctx.body = 'merry christmas');
+
+const app = new Koa();
+const router = new KoaRouter();
+
+app.use(json());
+
+// app.use(async ctx => ctx.body = { message: 'merry christmas' });
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false
+});
+
+router.get('/', async ctx => {
+  await ctx.render('index');
+});
+
+router.get('/test', ctx => (ctx.body = { test: 'Hello Test' }));
+
+// router middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, () => {
   console.log(`server listening on ${port}`);
