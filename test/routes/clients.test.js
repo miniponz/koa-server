6c56a97e368c3013d97c34beda1f3ec13c6827cc
Cara Ponzini <ponzini@gmail.com>
2019-05-17 10:28:11 -0700

    test for get

--- test/routes/clients.test.js
+++ test/routes/clients.test.js
@@ -3,7 +3,30 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const connect = require('../../lib/utils/connect');
 const app = require('../../lib/app');
-// const Client = require('../lib/models/Client');
+const Client = require('../../lib/Models/Client');
+
+const fakeClient = {
+  name: {
+    first: 'Caroline',
+    middle: 'J.',
+    last: 'Ponzini',
+    pref: 'Cara'
+  },
+  address: {
+    street: '555 NW Bond St.',
+    city: 'Bend',
+    state: 'OR',
+    zip: '97703'
+  },
+  telephone: [{
+    number: 5415551212,
+    phoneType: 'cell',
+    safe: true
+  }],
+  email: 'client@blah.net',
+  referredBy: 'your mom',
+  notes: 'this client is an elephant'
+};
 
 describe('client routes', () => {
   beforeAll(() => {
@@ -21,28 +44,7 @@ describe('client routes', () => {
   it('posts a client', () => {
     return request(app.callback())
       .post('/api/v1/clients')
-      .send({
-        name: {
-          first: 'Caroline',
-          middle: 'J.',
-          last: 'Ponzini',
-          pref: 'Cara'
-        },
-        address: {
-          street: '555 NW Bond St.',
-          city: 'Bend',
-          state: 'OR',
-          zip: '97703'
-        },
-        telephone: [{
-          number: 5415551212,
-          phoneType: 'cell',
-          safe: true
-        }],
-        email: 'client@blah.net',
-        referredBy: 'your mom',
-        notes: 'this client is an elephant'
-      })
+      .send(fakeClient)
       .then(res => {
         expect(res.body.data).toEqual({
           __v: 0,
@@ -72,4 +74,41 @@ describe('client routes', () => {
         });
       });
   });
+
+  it('gets all clients', () => {
+    return Client.create(fakeClient)
+      .then(() => {
+        return request(app.callback())
+          .get('/api/v1/clients')
+          .then(res => {
+            expect(res.body.data).toEqual([{
+              __v: 0,
+              name: {
+                first: 'Caroline',
+                middle: 'J.',
+                last: 'Ponzini',
+                pref: 'Cara'
+              },
+              address: {
+                street: '555 NW Bond St.',
+                city: 'Bend',
+                state: 'OR',
+                zip: '97703'
+              },
+              telephone: [{
+                _id: expect.any(String),
+                number: 5415551212,
+                phoneType: 'cell',
+                safe: true
+              }],
+              email: 'client@blah.net',
+              matters: [],
+              referredBy: 'your mom',
+              notes: 'this client is an elephant',
+              _id: expect.any(String)
+            }]);
+          });
+      });
+  });
+
 });
